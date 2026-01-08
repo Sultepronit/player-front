f715bc0aeb13e3f3dae4232c917065a26d4d8c8d
sultepronit <sultepronit@gmail.com>
2026-01-08 14:31:37 +0000

    basic fix? still debugging

--- src/playbackHandlers.js
+++ src/playbackHandlers.js
@@ -127,7 +127,7 @@ async function initiateFilesList() {
     try {
         const filenames = await restoreFilesList();
         // console.log(filenames);
-        // addMessage(filenames.join(', '));
+        addMessage(filenames.join(', '));
         localFiles = filenames.map(n => n.split('.')[0]);
         console.log(localFiles);
     } catch (error) {
@@ -207,6 +207,12 @@ async function setTrack(info, play = true) {
     try {
         // console.log(await deleteItem('files', '0.mp3'));
         const blob = await getLocalFile(info.filename);
+
+        if (!blob) {
+            const re = await deleteItem('files', info.filename);
+            if (re === 'success!') addMessage(info.filename, 'is removed!');
+            throw new Error('Non existing file!');
+        }
 
         URL.revokeObjectURL(audio.src);
         audio.src = URL.createObjectURL(blob);
